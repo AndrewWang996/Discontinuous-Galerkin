361f3a5a295ea5b377e38f0f2779c94713010165
Andy Wang <AndrewWang996@gmail.com>
2015-07-10 16:52:38 +0200

    completed integration with polynomial bounds, integration on any number of dimensions now possible

--- Polynomial.py
+++ Polynomial.py
@@ -40,6 +40,15 @@ class Polynomial:
 
 		return newPoly
 
+	def __neg__(self):
+		zipped = [(self.Monomials[expo], expo) for expo in self.Monomials]
+		coeffs, expos = zip( * zipped )
+		coeffs = [-c for c in coeffs]
+		return Polynomial(coeffs, expos)
+
+	def __sub__(self, poly):
+		return ( - poly ) + self
+
 	def __mul__(self, poly):
 		newPoly = Polynomial([], [])
 
@@ -58,14 +67,17 @@ class Polynomial:
 		return newPoly
 
 	def __pow__(self, n):
-		result = self.identity()
+		if n < 0:
+			raise Exception("Negative exponents not allowed.")
+		dim = len(self.Monomials.keys()[0])
+		result = self.identity(dim)
 		if n < 16:
 			for i in range(n):
 				result = result * self
 			return result
 
 		# use binary exponentiation
-		result = self.identity()
+		result = self.identity(dim)
 		base = self.copy()
 		while n > 0:
 			if (n & 1) > 0: 
@@ -75,8 +87,32 @@ class Polynomial:
 		return result
 
 
-	def identity(self):
-		return Polynomial([1], [ tuple([0] * dimension) ])
+	def integrate(self, L, U):   	# assuming on the last variable
+									# assuming L, U are polynomials
+		s = Polynomial([], [])
+
+		poly = self.copy()
+		length = -1
+		for expo in poly.Monomials:
+			coeff = poly.Monomials[expo]
+			if length < 0:
+				length = len(expo)
+			elif length != len(expo):
+				raise Exception("Lengths of monomials are different")
+			
+			lastExponent = expo[-1]
+			newExpo = tuple(expo[:-1])
+			coeff /= float(lastExponent + 1)
+
+			newPoly = (U ** (lastExponent + 1)) - (L ** (lastExponent + 1))
+			s += newPoly * Polynomial([coeff], [newExpo])
+
+		return s
+
+
+
+	def identity(self, dim):
+		return Polynomial([1], [ tuple([0] * dim) ])
 
 
 	def copy(self):
@@ -87,6 +123,22 @@ class Polynomial:
 			coeffs.append(self.Monomials[expo])
 
 		return Polynomial(coeffs, expos)
+
+
+
+# poly = Polynomial([1,1], [(1,0),(0,1)])
+# poly = poly.integrate(Polynomial([0],[(0,)]), Polynomial([1,-1], [(0,), (1,)]))
+# poly = poly.integrate(Polynomial([0],[()]), Polynomial([1], [()]))
+# print poly
+
+# poly += Polynomial([1], [(1,0)])
+# print poly
+
+# print poly ** 0
+# print poly ** 1
+# print poly ** 2
+
+# print poly - poly
 
 
 
