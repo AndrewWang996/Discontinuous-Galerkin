73c79bf5802e56daae74d79fa05c2db50c77cfe2
Andy Wang <AndrewWang996@gmail.com>
2015-07-20 18:07:26 +0200

    first commit for DampingMatrix generation

---
+++ getDampingMatrix.py
@@ -0,0 +1,7 @@
+'''
+Author: Andy Wang
+Description: Finds the Damping Matrix (the matrix in front
+	of the term with d/dt)
+	Currently there is none, because there is no Absorbing
+	Boundary Condition.
+'''
